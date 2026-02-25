2b4ced614d7bb5c39d973abc743c07b24d53b102
cyclopentane <cyclopentane@aidoskyneen.eu>
2026-02-25 22:09:44 +0100

    remove old AOSP modules in default.nix too
    oops.

--- default.nix
+++ default.nix
@@ -47,9 +47,6 @@ let
         #./flavors/vanilla/11/kernel
         #./flavors/vanilla/12
         #./flavors/waydroid
-        ./modules/10
-        ./modules/11
-        ./modules/12
         ./modules/13
         ./modules/15
         ./modules/16
